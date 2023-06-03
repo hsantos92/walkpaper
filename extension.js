9688cd854f60c581d4b0a92a04ffb020b61e684b
H <55033813+hsantos92@users.noreply.github.com>
2023-06-03 00:37:21 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,6 +1,4 @@
-const Gio = imports.gi.Gio;
-const Meta = imports.gi.Meta;
-
+const { Gio, Meta } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 
 const WORKSPACE_COUNT_KEY = 'workspace-count';
@@ -12,58 +10,58 @@ const CURRENT_WALLPAPER_KEY = 'picture-uri';
 let _settings;
 
 function debugLog(s) {
-    //log(s);
+//log(s);
 }
 
-function _changeWallpaper() {
-    debugLog("changeWallpaper");
-    let backgroundSettings = new Gio.Settings({ schema_id: BACKGROUND_SCHEMA });
-    let paths = _settings.get_strv(WALLPAPERS_KEY);
-    let index = _settings.get_int(WORKSPACE_INDEX);
+function changeWallpaper() {
+debugLog("changeWallpaper");
+const backgroundSettings = new Gio.Settings({ schema_id: BACKGROUND_SCHEMA });
+const paths = _settings.get_strv(WALLPAPERS_KEY);
+const index = _settings.get_int(WORKSPACE_INDEX);
 
-    debugLog("Walkpaper change from WS " + index);
+debugLog("Walkpaper change from WS " + index);
 
-    // Save wallpaper for previous WS if changed.
-    let wallpaper = backgroundSettings.get_string(CURRENT_WALLPAPER_KEY);
+// Save wallpaper for the previous WS if changed.
+const wallpaper = backgroundSettings.get_string(CURRENT_WALLPAPER_KEY);
 
-    paths[index] = wallpaper;
+paths[index] = wallpaper;
 
-    // Fill in empty entries up to to current, otherwise set_strv fails
-    for (let i=0; i < index; i++) {
-        if (typeof paths[i] === "undefined") {
-            paths[i] = wallpaper;
-        }
-    }
-    _settings.set_strv(WALLPAPERS_KEY, paths);
+// Fill in empty entries up to the current index, otherwise set_strv fails.
+for (let i = 0; i < index; i++) {
+if (typeof paths[i] === "undefined") {
+paths[i] = wallpaper;
+}
+}
+_settings.set_strv(WALLPAPERS_KEY, paths);
 
-    // Now get wallpaper for current workspace
-    index = global.workspace_manager.get_active_workspace_index();
-    debugLog("Walkpaper change to WS " + index);
+// Now get the wallpaper for the current workspace.
+const activeWorkspaceIndex = global.workspace_manager.get_active_workspace_index();
+debugLog("Walkpaper change to WS " + activeWorkspaceIndex);
 
-    wallpaper = paths[index];
-    if ((typeof wallpaper === "undefined") || (wallpaper == "")) {
-        wallpaper = paths[0];    // Default
-    }
-
-    //Change wallpaper
-    debugLog("Walkpaper set wallpaper to " + wallpaper);
-    backgroundSettings.set_string(CURRENT_WALLPAPER_KEY, wallpaper);
-
-    _changeIndex();
+let newWallpaper = paths[activeWorkspaceIndex];
+if ((typeof newWallpaper === "undefined") || (newWallpaper === "")) {
+newWallpaper = paths[0]; // Default
 }
 
-function _changeIndex() {
-    let index = global.workspace_manager.get_active_workspace_index();
-    _settings.set_int(WORKSPACE_INDEX, index);
+// Change wallpaper
+debugLog("Walkpaper set wallpaper to " + newWallpaper);
+backgroundSettings.set_string(CURRENT_WALLPAPER_KEY, newWallpaper);
+
+changeIndex();
 }
 
-function _workspaceNumChanged() {
-    let workspaceNum = Meta.prefs_get_num_workspaces();
-    _settings.set_int(WORKSPACE_COUNT_KEY, workspaceNum);
+function changeIndex() {
+const index = global.workspace_manager.get_active_workspace_index();
+_settings.set_int(WORKSPACE_INDEX, index);
+}
+
+function workspaceNumChanged() {
+const workspaceNum = Meta.prefs_get_num_workspaces();
+_settings.set_int(WORKSPACE_COUNT_KEY, workspaceNum);
 }
 
 function init(metadata) {
-    log("Walkpaper init");
+log("Walkpaper init");
 }
 
 let wSwitchedSignalId;
@@ -71,30 +69,32 @@ let wAddedSignalId;
 let wRemovedSignalId;
 
 function enable() {
-    log("Walkpaper enable");
+log("Walkpaper enable");
 
-    //Initialize globals
-    _settings = ExtensionUtils.getSettings();
+// Initialize globals
+_settings = ExtensionUtils.getSettings();
 
-    //Initialize settings values
-    _changeIndex();
-    _workspaceNumChanged();
+// Initialize settings values
+changeIndex();
+workspaceNumChanged();
 
-    //Connect signals
-    wSwitchedSignalId = global.workspace_manager.connect('workspace-switched', _changeWallpaper);
-    wAddedSignalId = global.workspace_manager.connect('workspace-added', _workspaceNumChanged);
-    wRemovedSignalId = global.workspace_manager.connect('workspace-removed', _workspaceNumChanged);
+// Connect signals
+wSwitchedSignalId = global.workspace_manager.connect('workspace-switched', changeWallpaper);
+wAddedSignalId = global.workspace_manager.connect('workspace-added', workspaceNumChanged);
+wRemovedSignalId = global.workspace_manager.connect('workspace-removed', workspaceNumChanged);
 }
 
 function disable() {
-    log("Walkpaper disable");
+log("Walkpaper disable");
 
-    //Dispose of globals
-    _settings?.run_dispose();
-    _settings = null;
+// Dispose of globals
+if (_settings) {
+_settings.run_dispose();
+_settings = null;
+}
 
-    //Disconnect signals
-    global.workspace_manager.disconnect(wSwitchedSignalId);
-    global.workspace_manager.disconnect(wAddedSignalId);
-    global.workspace_manager.disconnect(wRemovedSignalId);
+// Disconnect signals
+global.workspace_manager.disconnect(wSwitchedSignalId);
+global.workspace_manager.disconnect(wAddedSignalId);
+global.workspace_manager.disconnect(wRemovedSignalId);
 }
